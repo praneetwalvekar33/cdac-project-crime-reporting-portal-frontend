b6d6dfe8c81dc4ab6ca7b058bf4d8e8907a4521e
praneetwalvekar33 <praneetwalvekar@gmail.com>
2024-02-22 14:34:37 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import PoliceComplaintList from './component/police/complaintList/complaintList';
